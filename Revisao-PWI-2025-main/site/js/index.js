7c7c339abf8ddd6e2244b584d6571c1f7239dd92
bruna126lopes <brunasilvalopes629@gmail.com>
2025-02-24 22:17:57 -0300

    Add files via upload

---
+++ Revisao-PWI-2025-main/site/js/index.js
@@ -0,0 +1,30 @@
+function logar(event) {
+
+    event.preventDefault();
+
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+
+    if (usuario == 'admin' && senha == 'admin') {
+
+        Swal.fire({
+            title: 'Login realizado!',
+            text: 'Bem-vindo,' + usuario + '!',
+            icon: 'success',
+            confirmButttonText: 'OK'
+        }).then(() => {
+            setTimeout(() => {
+                location.href = "./html/home.html";
+            }, 100);
+        });
+    } else {
+      Swal.fire({
+        title: 'Erro!',
+        text: 'Usuário ou senha incorretos.',
+        icon: 'error',
+        confirmButttonText: 'Tentar novamente',
+
+      });
+
+    }
+}
